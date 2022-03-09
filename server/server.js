6e867a33fe86ba82079e81e090d6b6e1443eaa11
luca <lucafumarola96@gmail.com>
2022-03-10 00:35:20 +0100

    Standard Templtates

---
+++ server/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//import express
+const express = require('express');
+const morgan = require('morgan'); // logging middleware
+const jwt = require('express-jwt');
+const jsonwebtoken = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const jwtSecret = '6xvL4xkAAbG49hcXf5GIYSvkDICiUAR6EdR5dLdwW7hMzUjjMUe9t6M5kSAYxsvX';
+const expireTime = 300; //seconds
+// Authorization error
+const authErrorObj = { errors: [{  'param': 'Server', 'msg': 'Authorization error' }] };
+//create application
+const app = express();
+const port = 3001;
+
+
+app.listen(port, ()=>console.log(`Server running on http://localhost:${port}/`));
