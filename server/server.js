fb56121351160547aa5676bce3d3e1303a509880
luca <lucafumarola96@gmail.com>
2022-03-16 01:18:08 +0100

    Server update

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const morgan = require('morgan'); // logging middleware
 const jwt = require('express-jwt');
 const jsonwebtoken = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const mysql = require('mysql');
 const jwtSecret = '6xvL4xkAAbG49hcXf5GIYSvkDICiUAR6EdR5dLdwW7hMzUjjMUe9t6M5kSAYxsvX';
 const expireTime = 300; //seconds
 // Authorization error
@@ -14,5 +15,15 @@ const authErrorObj = { errors: [{  'param': 'Server', 'msg': 'Authorization erro
 const app = express();
 const port = 3001;
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password"
+  });
+  
+con.connect(function(err) {
+    if (err) console.log("Cannot establish a connection with Database");
+    else console.log("Connected!");
+  });
 
 app.listen(port, ()=>console.log(`Server running on http://localhost:${port}/`));
