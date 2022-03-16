50ea233e3c13058acf17735b8c26b0185c10e49b
luca <lucafumarola96@gmail.com>
2022-03-17 00:31:10 +0100

    Client update

---
+++ client/buttons.js
@@ -0,0 +1,12 @@
+let loginButton = document.getElementById("loginButton");
+let siginButton = document.getElementById("siginButton");
+
+loginButton.onclick = function(){
+    console.log("login Button pressed");
+    //check login + query al server
+}
+
+siginButton.onclick = function(){
+    console.log("sigin Button pressed");
+    //check sigin + query al server
+}
