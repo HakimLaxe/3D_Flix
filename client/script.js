b377dc614990b3c8f7fe885960bdef3b96e02bcc
angelo <alucarella.al@gmail.com>
2022-04-01 17:47:01 +0200

    oh pezzodemmerda

--- client/script.js
+++ client/script.js
@@ -1,3 +1,4 @@
+const { count } = require("console");
 const { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } = require("constants");
 
 function onCloseModal(){
@@ -236,6 +237,57 @@ function submitSigin(){
 
 //Slider Immagini Stampanti
 
-$('.carousel').slider({
-    interval: 1000
-  })
+var responsiveSlider = function() {
+
+    var slider = document.getElementById("slider");
+    var sliderWidth = slider.offsetWidth;
+    var slideList = document.getElementById("slideWrap");
+    var count = 1;
+    var items = slideList.querySelectorAll("li").length;
+    var prev = document.getElementById("prev");
+    var next = document.getElementById("next");
+    
+    window.addEventListener('resize', function() {
+      sliderWidth = slider.offsetWidth;
+    });
+}
+
+var nextSlide = function() {
+    if(count < items) {
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+    }
+    else if(count = items) {
+        slideList.style.left = "0px";
+    }
+};
+
+var prevSlide = function() {
+    if(count > 1) {
+        count = count - 2;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+    }
+    else if(count = 1) {
+        count = items - 1;
+        slideList.style.left = "-" + count * sliderWidth + "px";
+        count++;
+    }
+};
+
+next.addEventListener("click", function() {
+    nextSlide();
+});
+
+prev.addEventListener("click", function() {
+    prevSlide();
+});
+
+setInterval(function() {
+    nextSlide()
+}, 100);
+
+
+window.onload = function() {
+    responsiveSlider();
+}
